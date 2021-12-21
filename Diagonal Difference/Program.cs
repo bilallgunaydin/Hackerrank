7d2bddca65f9c3b8dfc1318edab0afbcb87461e7
Bilal Günaydın <bilallgunaydin@gmail.com>
2021-12-21 15:36:06 +0300

    Hackerrank

---
+++ Diagonal Difference/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+namespace hackrank_Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int max = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (n > 0)
+            {
+                int reminder = n % 2;
+                n = n / 2;
+                if (reminder == 1)
+                {
+                    count++;
+                }
+               else
+                {
+                    count = 0;
+                }
+                if (max < count)
+                {
+                    max = count;
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+
+
+        }
+        
+    }
+    
+
+}
+
---
+++ Plus Minus/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace hackrankdizichallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        int n = Convert.ToInt32(Console.ReadLine());
+        string inputs = Console.ReadLine();
+        string[] arr = inputs.Split(' ');
+
+        Result.plusMinus(arr, n);
+
+        }
+    }
+    class Result
+    {
+         public static void plusMinus(string[] arr, int n)
+    {
+        int neg=0; 
+        int pos=0;
+        int zero=0;
+            if( 0<n && n<=100)
+            {
+               for (int i = 0; i < n; i++)
+               {
+                   int newnumber=Convert.ToInt32(arr[i]);
+                   if(-100<=newnumber && newnumber<=100)
+                   {
+                       if(newnumber<0)
+                            neg=neg+1;
+                        else if(newnumber>0)
+                            pos=pos+1;
+                            else
+                            zero=zero+1;
+                   }
+               }
+            }
+            Decimal posResult=Convert.ToDecimal(pos)/Convert.ToDecimal(n);
+            Console.WriteLine("Positive result: "+posResult.ToString("0.000000"));
+            Decimal negResult=Convert.ToDecimal(neg)/Convert.ToDecimal(n);
+            Console.WriteLine("Negative result: "+negResult.ToString("0.000000"));
+            Decimal zeroResult=Convert.ToDecimal(zero)/Convert.ToDecimal(n);
+            Console.WriteLine("Zero result: "+zeroResult.ToString("0.000000"));
+
+    }
+    
+    }
+}
